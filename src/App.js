04c6db219e14071e5d0fec1e3cde4228f535ed45
ErkanSensei <erkansen14@hotmail.com>
2018-01-14 18:02:40 -0600

    added mobile projects section

--- src/App.js
+++ src/App.js
@@ -1,21 +1,93 @@
 import React from 'react';
 import $ from 'jquery';
 import Dropdown from 'react-dropdown';
-
-import Terminal from './components/Terminal';
-import Projects from './components/Projects';
-import Blog from './components/Blog';
-import Contact from './components/Contact';
-import Resume from './components/Resume';
+import Card from './components/Card'
+import IPhone from './components/IPhone'
+import Macbook from './components/Macbook'
 import 'react-dropdown/style.css';
 import './App.css';
 
-const components = {
-  Projects,
-  Blog,
-  Contact,
-  Resume,
-};
+const aoelogo = require('./assets/img/aoelogo.png');
+const erkanLogo = require('./assets/img/profile2.png');
+const ser = require('./assets/img/ser.png');
+const reactLogo = require('./assets/img/logo.svg');
+const hapsLogo = require('./assets/img/hapsLogo.png');
+const projects = [
+  // {
+  //   name: 'AOEDB.net',
+  //   image: aoelogo,
+  //   codeLink: 'https://github.com/ErkanSensei/AOEdb.net',
+  //   webLink: 'https://aoedb.net',
+  //   description: 'An overhaul of the original AOEdb.net website in Vue.js',
+  //   mainLanguage: 'Vue.js',
+  //   technologies: ['Vue', 'Vue-router', 'Bootstrap'],
+  //   platform: 'web',
+  // },
+  // {
+  //   name: 'erkansensei.com',
+  //   image: erkanLogo,
+  //   codeLink: 'https://github.com/ErkanSensei/erkansensei.github.io',
+  //   webLink: 'http://erkansensei.com',
+  //   description: 'My website! Built in React',
+  //   mainLanguage: 'React',
+  //   technologies: ['React', 'FontAwesome 5'],
+  //   platform: 'web',
+  // },
+  {
+    name: 'SwipeEatRepeat',
+    image: ser,
+    codeLink: null,
+    iosLink: '',
+    webLink: 'http://swipeeatrepeat.com',
+    description: 'An app to help people find new places to eat!',
+    mainLanguage: 'React',
+    technologies: ['React Native', 'Redux', 'Native Base'],
+    platform: 'iphone',
+  },
+  {
+    name: 'SwipeEatRepeat',
+    image: ser,
+    codeLink: null,
+    iosLink: '',
+    webLink: 'http://swipeeatrepeat.com',
+    description: 'An app to help people find new places to eat!',
+    mainLanguage: 'React',
+    technologies: ['React Native', 'Redux', 'Native Base'],
+    platform: 'iphone',
+  },
+  // {
+  //   name: 'SVG-to-ReactNativeSVG',
+  //   image: reactLogo,
+  //   codeLink: 'https://github.com/ErkanSensei/SVG-to-ReactNativeSVG',
+  //   webLink: 'http://erkansensei.com/SVG-to-ReactNativeSVG/',
+  //   description: 'A tool used to convert SVGs to ReactNative SVG',
+  //   mainLanguage: 'Javascript',
+  //   technologies: ['Javascript', 'HTML', 'CSS'],
+  //   platform: 'web',
+  // },
+  {
+    name: 'TheHAPS',
+    image: hapsLogo,
+    codeLink: '',
+    webLink: 'https://thehaps.live/',
+    iosLink: 'https://itunes.apple.com/us/app/thehaps/id1251645159?mt=8',
+    description: 'An app to find events nearby',
+    mainLanguage: 'React',
+    technologies: ['React Native', 'Redux', 'Sails'],
+    platform: 'iphone',
+  },
+  {
+    name: 'TheHAPS',
+    image: hapsLogo,
+    codeLink: '',
+    webLink: 'https://thehaps.live/',
+    iosLink: 'https://itunes.apple.com/us/app/thehaps/id1251645159?mt=8',
+    description: 'An app to find events nearby',
+    mainLanguage: 'React',
+    technologies: ['React Native', 'Redux', 'Sails'],
+    platform: 'iphone',
+  },
+];
 
 export default class App extends React.Component {
   constructor() {
@@ -25,18 +97,29 @@ export default class App extends React.Component {
     };
   }
 
-  onSelect = (text) => {
-      var elementClicked = $('#content');
-      var destination = $(elementClicked).offset().top;
-      $('html, body').animate({
-        scrollTop: parseInt($('#App').scrollTop() + destination),
+  componentDidMount() {
+    $(document).ready(() => {
+      $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+          $('#menu').fadeIn(500);
+        } else {
+          $('#menu').fadeOut(500);
+        }
       });
-      return;
+    });
+  }
+  onSelect = (text) => {
+    var elementClicked = $('#content');
+    var destination = $(elementClicked).offset().top;
+    $('html, body').animate({
+      scrollTop: parseInt($('#App').scrollTop() + destination),
+    });
   };
 
   render() {
     return (
       <div className='App' id='App'>
+        {/* <div id='menu'></div> */}
         <div className='textContainer'>
           <div className='innerContainer'>
             <p className='mainText'>HELLO</p>
@@ -52,7 +135,26 @@ export default class App extends React.Component {
             <p className='mainText'>DEVELOPER</p>
           </div>
         </div>
-        <div id='content'><p>HelloWorld!</p></div>
+        <div id='content'>
+          {projects.map(project => {
+            return (
+              <div className='projectContainer'>
+                <IPhone image={project.image}/>
+                <Card
+                name={project.name}
+                codeLink={project.codeLink}
+                webLink={project.webLink}
+                description={project.description}
+                mainLanguage={project.mainLanguage}
+                technologies={project.technologies}
+                arrow='arrow-left'
+                iosLink={project.iosLink}
+                platform={project.platform}
+              />
+              </div>
+            )
+          })}
+        </div>
       </div>
     );
   }
